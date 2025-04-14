64179f3f43be4e532699a2bb036008afe0b2a31c
Aravind S <aravinds90g@gmail.com>
2025-04-14 14:28:26 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/gatepass", gatePassRoutes);
 app.use("/api/mentor", mentorRoutes);
 app.use("/",( req, res) => {
-    res.send("Welcome to GatePass API")
+    res.status(200).json({ message: "Welcome to the GatePass API" });
 })
 
 
