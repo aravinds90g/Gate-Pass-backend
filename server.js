cd58a7830b03dbbe2f762bad7555a94128dc9943
Aravind S <aravinds90g@gmail.com>
2025-04-14 14:38:32 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/gatepass", gatePassRoutes);
 app.use("/api/mentor", mentorRoutes);
-app.use("/",( req, res) => {
-    res.status(200).json({ message: "Welcome to the GatePass API" });
-})
+app.use("/", (req, res) => res.json({ message: "Coupon Service" }));
 
 
 const PORT = process.env.PORT || 5000;
